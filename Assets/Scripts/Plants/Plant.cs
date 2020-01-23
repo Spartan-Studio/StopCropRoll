92e07d65628daa075cfdd7859545551507b5f5bb
Braeden Hunt <bjhunt918@gmail.com>
2020-01-23 11:44:10 -0700

    Added Item and Plant base classes

---
+++ Assets/Scripts/Plants/Plant.cs
@@ -0,0 +1,21 @@
+﻿public abstract class Plant
+{
+    public string name { get; set; }
+
+    public PlantType plantType { get; }
+    public int buyValue { get; set; }
+
+    public int sellValue { get; set; }
+
+    public float growthRate { get; set; }
+
+    public float targetValue { get; set; }
+
+}
+
+public enum PlantType
+{
+    Health,
+    Buff,
+    Sellable
+}
