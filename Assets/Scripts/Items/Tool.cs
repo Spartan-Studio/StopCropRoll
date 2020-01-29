7b836ee50fea0ac284f559cb9c297f20e650b68c
Braeden Hunt <bjhunt918@gmail.com>
2020-01-29 13:41:29 -0700

    Added Tool Class

---
+++ Assets/Scripts/Items/Tool.cs
@@ -0,0 +1,16 @@
+﻿public class Tool : Item
+{
+    public float cropMultiplier { get; set; }
+
+    public float seedMultiplier { get; set; }
+
+    public Tool(string name, int ammo, int capacity, float cropMultipler, float seedMultiplier)
+    {
+        this.name = name;
+        this.breakable = false;
+        this.durability = ammo;
+        this.maxDurability = capacity;
+        this.cropMultiplier = cropMultiplier;
+        this.seedMultiplier = seedMultiplier;
+    }
+}
