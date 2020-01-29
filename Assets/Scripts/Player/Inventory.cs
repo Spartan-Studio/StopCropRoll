07e96f595500c7b886ceceffde2f21492aaa8ca8
Braeden Hunt <bjhunt918@gmail.com>
2020-01-29 14:58:07 -0700

    Added Inventory and Corn

---
+++ Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Generic;
+
+public class Inventory
+{
+    private IDictionary<string, int> inventory;
+
+    public Inventory()
+    {
+        inventory = new Dictionary<string, int>();
+    }
+
+    public int getInventoryAmount(string plantName)
+    {
+        return inventory[plantName];
+    }
+
+    public void addInventortAmount(string plantName, int amount)
+    {
+        inventory[plantName] += amount;
+    }
+}
