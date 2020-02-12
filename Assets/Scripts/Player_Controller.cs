f3e57fbafd761fdf7faf116efd42e7f129553c1e
Ira Williams <iawill6220@gmail.com>
2020-02-12 14:55:43 -0700

    Demo Imported

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Controller : MonoBehaviour
+{
+
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
+
+    public float runSpeed = 20.0f;
+    public float health;
+    private Animator anim;
+    public Slider healthbar;
+
+
+    void Start()
+    {
+        health = 10f;
+        anim = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Gives a value between -1 and 1
+        horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
+        vertical = Input.GetAxisRaw("Vertical"); // -1 is down
+
+        healthbar.value = health;
+
+        if (health <= 0f)
+        {
+            health = 0f;
+        }
+
+
+        if (horizontal == 0f && vertical == 0f)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            anim.SetBool("isRunning", true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+
+    void OnCollisionEnter2D(Collision2D enemy)
+    {
+        if (enemy.gameObject.tag == "EnemySmall")
+        {
+            health = health -2;
+        }
+    }
+
+}
