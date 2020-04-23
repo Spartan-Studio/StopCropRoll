8b5acd551632ff9a2cff3b355614abb6106189c0
Braeden Hunt <20hunbra@student.mcpsmt.org>
2020-04-23 13:20:23 -0600

    Added Scriptable Object Plant and Corn

---
+++ Assets/Scriptable Objects/Plant.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Plant", menuName = "Plant")]
+public class Plant : ScriptableObject
+{
+    public new string name;
+
+    public PlantType plantType;
+
+    public int buyValue;
+
+    public int sellValue;
+
+    public float growthRate;
+
+    public float targetValue;
+
+    public float hitpoints;
+
+    public Sprite sprite;
+
+}
+
+public enum PlantType
+{
+    HEALTH, BUFF, SELLABLE
+}
