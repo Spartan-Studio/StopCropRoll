d80b9232934e56a34598d03ed1bbb3c453044b5b
Braeden Hunt <20hunbra@student.mcpsmt.org>
2020-04-23 13:56:56 -0600

    Restored Scripts. Zombie Prefab Broken

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,10 @@
+﻿public abstract class Item
+{
+    public string name { get; set; }
+    
+    public int durability { get; set; }
+
+    public int maxDurability { get; set; }
+
+    public bool breakable { get; set; }
+}
---
+++ Assets/Scripts/Plants/Corn.cs
@@ -0,0 +1,10 @@
+﻿public class Corn : Plant
+{
+    public Corn()
+    {
+        this.name = "Corn";
+        this.plantType = PlantType.SELLABLE;
+        this.sellValue = 10;
+        this.buyValue = 7;
+    }
+}
