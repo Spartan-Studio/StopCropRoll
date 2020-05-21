aeaa3cef733cfd22d0d0d4c344ddd73a3f2021fd
Braeden Hunt <20hunbra@student.mcpsmt.org>
2020-05-20 18:47:28 -0600

    Added UI for the inventory

--- Assets/Scripts/Player/PlantInventoryContainer.cs
+++ Assets/Scripts/Player/PlantInventoryContainer.cs
@@ -1,21 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlantInventoryContainer : MonoBehaviour
 {
     public PlantInventory inventory;
+    private Text invetoryText;
 
     // Start is called before the first frame update
     void Start()
     {
         inventory = new PlantInventory();
         Debug.Log("Inventory: " + inventory);
+        invetoryText = GameObject.FindGameObjectWithTag("InventoryText").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        invetoryText.text = "Corn: " + inventory.getPlantAmount(inventory.selectedPlant);
     }
 }
